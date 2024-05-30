2c03a478e7b266d5b9ee1c6ff81e2ac7a5fb39bc
jeevanandham010 <jeevakathirvel3699@gmail.com>
2024-05-30 06:19:58 +0530

    commit

--- src/main/java/com/example/SpringSecuritynew/config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecuritynew/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.SpringSecuritynew.config;
 
+import com.example.SpringSecuritynew.filter.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +13,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,11 +21,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    JwtFilter jwtFilter;
     @Bean
     UserDetailsService userDetailsService(){
 //        UserDetails admin= User.withUsername("admin")
@@ -47,10 +54,15 @@ public class SecurityConfig {
                        .requestMatchers("/kj/add").permitAll()
                        .requestMatchers("/kj/a").permitAll()
                        .anyRequest().permitAll()
-               )
-               .formLogin(Customizer.withDefaults()).
-        httpBasic(Customizer.withDefaults()).logout(Customizer.withDefaults());
-       return http.build();
+               ).sessionManagement(session->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtFilter,UsernamePasswordAuthenticationFilter.class)
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .logout(Customizer.withDefaults());
+        return http.build();
+
 
 
     }
---
+++ src/main/java/com/example/SpringSecuritynew/filter/JwtFilter.java
@@ -0,0 +1,43 @@
+package com.example.SpringSecuritynew.filter;
+
+import com.example.SpringSecuritynew.Service.JwtService;
+import com.example.SpringSecuritynew.config.UserDetaildemo;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    @Autowired
+    JwtService jwtService;
+    @Autowired
+    UserDetaildemo userDetaildemo;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader=request.getHeader("Authorization");
+        String token=null;
+        String username=null;
+        if(authHeader!=null && authHeader.startsWith("Bearer")){
+            token=authHeader.substring(7);
+            username=jwtService.extractusername(token);
+        }
+        if(username!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails=userDetaildemo.loadUserByUsername(username);
+            if(jwtService.validateToken(token,userDetails)){
+                UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+}
